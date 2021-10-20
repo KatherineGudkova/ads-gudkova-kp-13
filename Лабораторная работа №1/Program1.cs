0d56ce81bc007710b2859535c983bb077f43128f
Gudkova Katerina <92868372+KatherineGudkova@users.noreply.github.com>
2021-10-20 17:37:49 +0300

    Add files via upload

---
+++ Лабораторная работа №1/Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+class Program
+{
+    static void Main()
+    {
+        double x, y, z;
+        double ch, zn;
+        double a, b;
+
+        Write("x = ");
+        x = Convert.ToDouble(ReadLine());
+
+        Write("y = ");
+        y = Convert.ToDouble(ReadLine());
+
+        Write("z = ");
+        z = Convert.ToDouble(ReadLine());
+
+        ch = y - Math.Sqrt(Math.Abs(Math.Pow(x, 3)));
+        zn = Math.Sqrt(Math.Pow(x, 3) + 5 * Math.Pow(y, -z) + Math.Pow(z, 2));
+
+        if (zn == 0)
+            WriteLine("Error.");
+        else
+        {
+            a = ch / zn;
+            WriteLine("a =" + a);
+
+            b = Math.Sin(Math.Pow(a, -x)) + y;
+            WriteLine("b =" + b);
+        }
+        ReadLine();
+    }
+}
