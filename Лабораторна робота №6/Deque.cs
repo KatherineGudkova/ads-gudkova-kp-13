33e5aef55003a7b8bcfbebf34965d5a31f948e1b
Gudkova Katerina <92868372+KatherineGudkova@users.noreply.github.com>
2022-06-26 20:28:40 +0300

    Add files via upload
    Випадково виконала варіант 12, як і в роботі №5. Дякую за розуміння.

---
+++ Лабораторна робота №6/Deque.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using static System.Console;
+
+namespace Лабораторна6
+{
+    public class Node
+    {
+        public char data;
+        public Node next;
+        public Node prev;
+        public Node(char data)
+        {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+    public class Deque
+    {
+        private Node Head;
+        private Node Tail;
+        private int Size;
+        public Deque()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+        public bool isEmpty()
+        {
+            if (this.Head == null)
+                return true;
+            else
+                return false;
+        }
+        public void insertHead(char buffer)
+        {
+            Node node = new Node(buffer);
+            node.next = this.Head;
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                this.Head.prev = node;
+                this.Head = node;
+            }
+            this.Size++;
+            this.printDeque();
+        }
+        public Deque Copy()
+        {
+            Deque clone = new Deque();
+            Node node = this.Head;
+            while (node != null)
+            {
+                clone.insertTail(node.data);
+                node = node.next;
+            }
+            return clone;
+        }
+        public void insertTail(char buffer)
+        {
+            Node node = new Node(buffer);
+            if (this.Head == null)
+            {
+                this.Head = node;
+                this.Tail = node;
+            }
+            else
+            {
+                node.prev = this.Tail;
+                this.Tail.next = node;
+                this.Tail = node;
+            }
+            this.Size++;
+            this.printDeque();
+        }
+        public void removeTail()
+        {
+            if (this.isEmpty() == true)
+            {
+                return;
+            }
+            this.Tail = this.Tail.prev;
+
+            if (this.Tail == null)
+            {
+                this.Head = null;
+            }
+            else
+            {
+                this.Tail.next = null;
+            }
+            this.Size--;
+
+            this.printDeque();
+        }
+        public void removeHead()
+        {
+            if (this.isEmpty() == true)
+            {
+                return;
+            }
+            this.Head = this.Head.next;
+            if (this.Head == null)
+            {
+                this.Tail = null;
+            }
+            else
+            {
+                this.Head.prev = null;
+            }
+            this.Size--;
+            this.printDeque();
+        }
+
+        public Node head() => this.Head;
+
+        public Node tail() => this.Tail;
+        public int size() => this.Size;
+        public void printDeque()
+        {
+            Node node = this.Head;
+            WriteLine();
+            if (node != null)
+            {
+                while (node != null)
+                {
+                    Write(node.data);
+                    node = node.next;
+                }
+            }
+            else
+                WriteLine("\nДаний дек - порожнiй.");
+        }
+        public bool isPalindrome()
+        {
+            WriteLine("\n\nСтворення копiї дека:");
+            Deque current = this.Copy();
+
+            if (!current.isEmpty())
+            {
+                WriteLine("\n\nВидалення елементiв з копiї:");
+                do
+                {
+                    if (current.Head.data == current.Tail.data)
+                    {
+                        current.removeTail();
+                        current.removeHead();
+                    }
+                    else
+                        return false;
+                }
+                while (!current.isEmpty());
+                return true;
+            }
+            else return false;
+        }
+        public void printHalfDeque()
+        {
+            string half = "";
+            WriteLine("\n\nСтворення копiї дека:");
+            Deque current = this.Copy();
+
+            WriteLine("\n\nВидалення елементiв з копiї");
+            while (!current.isEmpty())
+            {
+                half += current.Tail.data;
+
+                current.removeHead();
+                current.removeTail();
+            }
+            WriteLine($"\n\nПоловина деку: {half}");
+        }
+        public void printTransposition()
+        {
+            WriteLine("\nПерестановка елементiв: ");
+            for (int i = 0; i < this.Size / 2; i++)
+            {
+                this.insertHead(this.Tail.data);
+                this.removeTail();
+            }
+            this.printDeque();
+        }
+    }
+}
---
+++ Лабораторна робота №6/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using static System.Console;
+
+namespace Лабораторна6
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Deque q = new Deque();
+            string command = "Enter", example = "asdffdsa";
+            string Input;
+            bool result = false;
+
+            while (command != "End")
+            {
+                WriteLine("\nГоловне меню");
+                WriteLine("Введiть одне з наступних ключових слiв:");
+                WriteLine("Example - вивести контрольний приклад");
+                WriteLine("Enter - виконати перетворення рядка");
+                WriteLine("End - завершити виконання програми");
+                command = ReadLine();
+
+                if (command == "Enter")
+                {
+                    do
+                    {
+                        q = new Deque();
+                        WriteLine("\nВведiть рядок: ");
+                        Input = ReadLine();
+
+                        while (Input.Length % 2 != 0 || Input.Length == 0)
+                        {
+                            WriteLine("\nЧисло символiв має бути кратне двом. Введiть iнший рядок: ");
+                            Input = ReadLine();
+                        }
+
+                        StringToDeque(q, Input);
+                        result = q.isPalindrome();
+
+                        if (!result)
+                        {
+                            WriteLine("\nДаний рядок не палiндром. \nВведiть iнший рядок: ");
+                        }
+                    }
+                    while (!result);
+                    printResult(q);
+                }
+
+                else if (command == "Example")
+                {
+                    WriteLine("Рядок-приклад: " + example);
+
+                    StringToDeque(q, example);
+
+                    if (q.isPalindrome())
+                        printResult(q);
+                    else
+                    {
+                        WriteLine("\nДаний рядок - не палiндром");
+                    }
+                }
+
+                else if (command == "End")
+                    break;
+                else
+                {
+                    WriteLine("\nПомилка при введеннi даних");
+                }
+            }
+        }
+        static public void printResult(Deque q)
+        {
+            WriteLine("\nДаний рядок - палiндром.");
+            q.printHalfDeque();
+            q.printTransposition();
+            WriteLine();
+        }
+        static public void StringToDeque(Deque q, string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                q.insertTail(input[i]);
+            q.printDeque();
+        }
+    }
+}
